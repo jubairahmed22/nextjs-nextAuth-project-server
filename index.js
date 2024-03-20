063e0e116c829afd2ee6db8bb5fb080204f4f138
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-20 14:39:42 +0600

    added something

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function run() {
       if (existingUser) {
         return res.status(400).json({
           success: false,
-          message: "User already exist!!!",
+          message: "User already exists!!!",
         });
       }
 
